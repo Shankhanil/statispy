ad9e60f852f6d53b5fb0015584e543c7444fef53
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-05-20 00:03:06 +0530

    setup repo

---
+++ setup.py
@@ -0,0 +1,14 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+setup(
+    name="statispy",
+    version="0.1.0",
+    author="Shankhanil Ghosh",
+    author_email="shankha.rik@gmail.com",
+    packages=find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
+)
