a00839efcadfa1bfcba1ee7b2f45523975aad375
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-05-21 14:44:20 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="statispy",
-    version="0.1.0",
+    version="0.2.0",
     author="Shankhanil Ghosh",
     author_email="shankha.rik@gmail.com",
     packages=find_packages(),
